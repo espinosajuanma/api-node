d9ffb0c30c07fc614a3d0f70d9399f20a7c1fad5
Juanma Espinosa <espinosajuanmanuel@gmail.com>
2022-09-09 22:53:14 -0300

    Added `200` HTTP statuses

--- src/server/status.js
+++ src/server/status.js
@@ -2,3 +2,16 @@ module.exports.OK = { code: 200, message: 'Ok' }
 module.exports.NOT_FOUND = { code: 404, message: 'Not found' }
 module.exports.SERVER_ERROR = { code: 500, message: 'Server error' }
 
+module.exports.CREATED = { code: 201, message: 'Created' }
+module.exports.ACCEPTED = { code: 202, message: 'Accepted' }
+module.exports.NON_AUTHORITATIVE_INFORMATION = { code: 203, message: 'Non-Authoritative information' }
+module.exports.NO_CONTENT = { code: 204, message: 'No content' }
+module.exports.RESET_CONTENT = { code: 205, message: 'Reset content' }
+module.exports.PARTIAL_CONTENT = { code: 206, message: 'Partial content' }
+module.exports.MULTI_STATUS = { code: 207, message: 'Multi-status' }
+module.exports.ALREADY_REPORTED = { code: 208, message: 'Already reported' }
+module.exports.IM_USED = { code: 226, message: 'IM used' }
+
+module.exports.MULTIPLE_CHOICES = { code: 300, message: 'Multiple choices' }
+module.exports.MOVED_PERMANENTLY = { code: 301, message: 'Moved Permanently' }
+
